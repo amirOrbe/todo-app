d048575e461a7a14a72119d704b8e79f93bbe01d
amirOrbe <amir.orbe@bunsan.io>
2023-01-04 12:15:25 -0600

    Add todo app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
+<script setup>
+import { ref } from "vue";
+const newTodoItem = ref("");
+const todo = ref([]);
+const addTodoItem = () => {
+  todo.value.push({
+    id: todo.value.length + 1,
+    task: newTodoItem.value,
+    done: false,
+  });
+  newTodoItem.value = "";
+};
+</script>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container p-3">
+    <h1 class="display-3">Welcome to your Todo List</h1>
+
+    <div class="input-group mb-3">
+      <input
+        v-model="newTodoItem"
+        type="text"
+        class="form-control"
+        placeholder="Add a todo"
+        aria-label="Todo input"
+        aria-describedby="Todo input"
+      />
+      <div v-if="newTodoItem">
+        <button
+          class="btn btn-outline-secondary"
+          type="button"
+          id="button-add-todo-item"
+          v-on:click="addTodoItem"
+        >
+          Add
+        </button>
+      </div>
+    </div>
+  </div>
+  <todoList :todoItems="todo" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todoList from "./components/todoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    todoList,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <ul class="list-group">
+      <small class="text-danger" v-if="todoItems.length == 0"
+        >Empty list please add a todo</small
+      >
+      <li class="list-group-item" v-for="todoItem in todoItems" :key="todoItem">
+        <div class="row">
+          <div class="col-sm">
+            <span class="text-dark">{{ todoItem.id }}</span>
+          </div>
+          <div class="col-sm mb-0">
+            <p
+              :class="[
+                todoItem.done ? 'text-decoration-line-through' : '',
+                'text-break',
+              ]"
+            >
+              {{ todoItem.task }}
+            </p>
+          </div>
+          <div class="col-sm">
+            <input v-model="todoItem.done" type="checkbox" />
+            <label class="form-check-label text-muted"> Mark as Done </label>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoLists",
+  props: {
+    todoItems: Array,
+  },
+};
+</script>
